9331704adad8cba951bbdef9c11813937768fce9
王正东 <wangzhengdong@ecmoban.com>
2020-12-25 15:07:17 +0800

    更新验证码应用安装器类名

--- classes/Installer.php
+++ classes/Installer.php
@@ -1,43 +1,77 @@
 <?php
-defined('IN_ECJIA') or exit('No permission resources.');
 
-class captcha_installer  extends ecjia_installer {
-    
+namespace Ecjia\App\Captcha;
+
+use ecjia;
+use ecjia_config;
+use ecjia_installer;
+
+class Installer extends ecjia_installer
+{
+
     protected $dependent = array(
-    	'ecjia.system'    => '1.0',
+        'ecjia.system' => '1.0',
     );
-    
-    public function __construct() {
+
+    public function __construct()
+    {
         $id = 'ecjia.captcha';
         parent::__construct($id);
     }
-    
-    
-    public function install() {
+
+    /**
+     * 安装
+     * @return bool
+     */
+    public function install()
+    {
         if (!ecjia::config('captcha', ecjia::CONFIG_CHECK)) {
             ecjia_config::instance()->insert_config('hidden', 'captcha', '', array('type' => 'hidden'));
         }
+
         if (!ecjia::config('captcha_width', ecjia::CONFIG_CHECK)) {
             ecjia_config::instance()->insert_config('hidden', 'captcha_width', '', array('type' => 'hidden'));
         }
+
         if (!ecjia::config('captcha_height', ecjia::CONFIG_CHECK)) {
             ecjia_config::instance()->insert_config('hidden', 'captcha_height', '', array('type' => 'hidden'));
         }
+
+        return true;
     }
-    
-    
-    public function uninstall() {
+
+    /**
+     * 卸载
+     * @return bool
+     */
+    public function uninstall()
+    {
         if (ecjia::config('captcha', ecjia::CONFIG_CHECK)) {
             ecjia_config::instance()->delete_config('captcha');
         }
+
         if (ecjia::config('captcha_width', ecjia::CONFIG_CHECK)) {
             ecjia_config::instance()->delete_config('captcha_width');
         }
+
         if (ecjia::config('captcha_height', ecjia::CONFIG_CHECK)) {
             ecjia_config::instance()->delete_config('captcha_height');
         }
+
+        return true;
     }
-    
+
+    /**
+     * 升级
+     * @return bool
+     */
+    public function upgrade()
+    {
+
+        return true;
+    }
+
+
 }
 
 // end
