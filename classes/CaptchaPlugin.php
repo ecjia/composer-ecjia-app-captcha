26b3ec04075434f666a2968b640ae0593d4cd86d
王正东 <wangzhengdong@ecmoban.com>
2020-12-25 15:07:44 +0800

    添加验证码插件的抽象类和管理类

---
+++ classes/CaptchaPlugin.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace Ecjia\App\Captcha;
+
+
+use Ecjia\Component\Plugin\PluginNoModel;
+
+class CaptchaPlugin extends PluginNoModel
+{
+
+    /**
+     * 获取数据中的Config配置数据，并处理
+     */
+    public function configData($code)
+    {
+
+    }
+
+
+}
